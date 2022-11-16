dead2f9d06d5198d95c645794fa0240d1c9d5851
Rajin Sharwar <rajinsharwar@gmail.com>
2022-11-16 17:20:26 +0600

    Adding the initial plugin files

---
+++ darkbuddy.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: DarkBuddy | Dark Mode for BuddyBoss Theme
+ * Description: Turn your BuddyBoss powered website into a beautiful eye-pleasing dark mode with a click. Simply activate this plugin, and your website will have it's beautiful Dark Mode ready.
+ * Author: Rajin Sharwar
+ * Author URI: https://linkedin.com/in/raiinsharwar
+ * Version: 1.0.0
+ */
+
+function darkbuddy_enqueue_css() {
+    $file_url = plugins_url('/css/main.css', __FILE__);
+    wp_enqueue_style( 'darkbuddy_css', $file_url);
+}
+
+add_action('wp_enqueue_scripts', 'darkbuddy_enqueue_css' );
+
+
+function darkbuddy_enqueue_js() {
+    $file_url = plugins_url('/js/main.js', __FILE__);
+    wp_enqueue_style( 'darkbuddy_js', $file_url);
+}
+
+add_action('wp_enqueue_scripts', 'darkbuddy_enqueue_js' );
---
+++ js/main.js
@@ -0,0 +1,26 @@
+javascript: (
+function darkbuddy() { 
+
+var darkbuddycss = 'html {-webkit-filter: invert(100%);' +
+    '-moz-filter: invert(100%);' + 
+    '-o-filter: invert(100%);' + 
+    '-ms-filter: invert(100%); }',
+
+head = document.getElementsByTagName('head')[0],
+style = document.createElement('style');
+
+
+if (!window.counter) { window.counter = 1;} else  { window.counter ++;
+if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}
+ };
+
+style.type = 'text/css';
+if (style.styleSheet){
+style.styleSheet.cssText = darkbuddycss;
+} else {
+style.appendChild(document.createTextNode(darkbuddycss));
+}
+
+//injecting the css in the site
+head.appendChild(style);
+}());               
